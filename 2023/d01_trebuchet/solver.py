41e1ddc8417747c44e585651063cf6423ca4be81
Mickael <rialland.mickael@gmail.com>
2023-12-01 09:03:10 +0100

    Advent of code - day 01

--- 2023/d01_trebuchet/solver.py
+++ 2023/d01_trebuchet/solver.py
@@ -2,7 +2,7 @@
 Author: Mickaël
 Date: 01/12/2023
 
-https://adventofcode.com/2023/day/1
+Solving https://adventofcode.com/2023/day/1
 """
 import time
 from pathlib import Path
@@ -13,20 +13,20 @@ SCRIPT_DIR = Path(__file__).parent
 INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
 
 def main():
-    print(f"Part 1: only digits > value={part_one()}")
-    print(f"Part 2: spelled with letters > value={part_two()}")
+    print(f"Part 1: {part_one()}")
+    print(f"Part 2: {part_two()}")
 
 def part_one():
-    return sum(map(concat_first_and_last_digits, open(INPUT_FILE)))
+    return sum(map(concat_first_and_last_digits, open(INPUT_FILE, mode="rt")))
 
 def concat_first_and_last_digits(line):
     x = re.findall(r'(\d)', line)
     return int(x[0] + x[-1])
 
 def part_two():
-    return sum(map(extract_digits_with_spelled, open(INPUT_FILE)))
+    return sum(map(convert_and_concat_first_and_last_digits, open(INPUT_FILE, mode="rt")))
     
-def extract_digits_with_spelled(line):
+def convert_and_concat_first_and_last_digits(line):
     for i, n in enumerate(['one','two','three','four','five','six','seven','eight','nine']):
         line = line.replace(n, n + str(i+1) + n)
     return concat_first_and_last_digits(line)
