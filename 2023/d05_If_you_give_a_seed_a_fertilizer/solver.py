57b8eb16c2a2894d4171a93d68097de4aeac7219
Mickael <rialland.mickael@gmail.com>
2023-12-05 21:48:59 +0100

    Advent of code - Day 05

--- 2023/d05_If_you_give_a_seed_a_fertilizer/solver.py
+++ 2023/d05_If_you_give_a_seed_a_fertilizer/solver.py
@@ -1,43 +1,75 @@
 """
 Author: Mickaël
-Date: 04/12/2023
+Date: 05/12/2023
 
-Solving https://adventofcode.com/2023/day/4
+Solving https://adventofcode.com/2023/day/5
 """
 import time
 from pathlib import Path
-import functools
+import math
 
 SCRIPT_DIR = Path(__file__).parent
 #INPUT_FILE = Path(SCRIPT_DIR, "input/sample_input.txt")
 INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
 
-def todo(val, m):
-    _, *ranges = m.split('\n')
-    for r in ranges:
-        dst, src, n=map(int, r.split())
-        if src <= val < src+n:
-            return val-src+dst
-    else: return val
+def parse_datas():
+    with open(INPUT_FILE, mode="rt") as f:
+        datas = [x.strip('\n') for x in f.readlines()]
+
+    seeds = [int(x) for x in datas[0].split(':')[1].split()]
+
+    maps = []
+    curr_map = []
+    for d in datas[3:]:
+        if d == '':
+            continue
+        if ':' in d:
+            maps += [curr_map]
+            curr_map = []
+        else:
+            curr_map += [tuple(int(x) for x in d.split(' '))]
+
+    maps += [curr_map]
+    return seeds, maps
+
+
+def apply_maps(seed, maps):
+    val = seed
+    for map in maps:
+        # ds = destination start
+        # ss = source start
+        # rl = range lenght
+        for ds, ss, rl in map:
+            if ss <= val < ss + rl:
+                val = ds + (val - ss)
+                break
+    return val
 
 def part_one(seeds, maps):
-    return min(functools.reduce(todo, maps, int(s)) for s in seeds)
+    seeds_location = {apply_maps(s, maps): s for s in seeds}
+    return min(seeds_location.keys())
 
 def part_two(seeds, maps):
-    pairs = list(zip(seeds[::2], seeds[1::2]))
-    locations = []
+    seeds = [(seeds[2 * i], seeds[2 * i + 1]) for i in range(len(seeds) // 2)]
 
-    # Pas optimisé du tout, à revoir !!!
-    for start, length in pairs:
-        seed_range = [*range(start, start+length,1)]
-        locations.append(part_one(seed_range, maps))
+    step_size = int(pow(10, math.ceil(math.log10(max(s[1] for s in seeds) / 100))))
+    search_vals = {(ss, ss + sl, s): apply_maps(s, maps) for ss, sl in seeds for s in range(ss, ss + sl, step_size)}
+    rough_est = min(search_vals.items(), key = lambda x: x[1])
 
-    return min(locations)
+    seed_range_start, seed_range_end, best_est = rough_est[0]
+
+    while step_size > 1:
+        left_search  = max(best_est - step_size, seed_range_start)
+        right_search = min(best_est + step_size, seed_range_end)
+
+        step_size = step_size // 10
+        search_vals = {s: apply_maps(s, maps) for s in range(left_search, right_search, step_size)}
+        best_est, best_loc = min(search_vals.items(), key = lambda x: x[1])
+    
+    return best_loc
 
 def main():
-    seeds, *maps = open(INPUT_FILE, mode="rt").read().split('\n\n')
-    seeds = [int(x) for x in seeds.split()[1:]]
-
+    seeds, maps = parse_datas()
     print(f"Part 1: {part_one(seeds, maps)}")
     print(f"Part 2: {part_two(seeds, maps)}")
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Advent of code
 
-Advent of Code problems solved in Python.
+Welcome to this GitHub repository! 🐍
+
+This repository is dedicated to enhancing my Python skills. Here, you'll find a collection code snippets that reflect my journey of learning and mastering Python during the advent of code adventure. Whether it's solving problems, exploring libraries, or experimenting with new concepts, this repository serves as a testament to my continuous growth as a Python developer.
+
+Feel free to explore the code, contribute, or share your insights. 
